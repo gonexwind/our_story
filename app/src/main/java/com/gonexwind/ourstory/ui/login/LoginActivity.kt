b2347a907c9ef91dea420d2964d2739530cb98f1
gonexwind <fikky.ardi15@gmail.com>
2022-04-04 21:35:33 +0700

    add icon launcher

--- app/src/main/java/com/gonexwind/ourstory/ui/login/LoginActivity.kt
+++ app/src/main/java/com/gonexwind/ourstory/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.gonexwind.ourstory
+package com.gonexwind.ourstory.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gonexwind.ourstory.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
