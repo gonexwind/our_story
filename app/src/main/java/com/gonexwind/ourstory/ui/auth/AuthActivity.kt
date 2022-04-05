dd49f099f95df494446c3a0f8c5c0d027d586350
gonexwind <fikky.ardi15@gmail.com>
2022-04-05 09:26:58 +0700

    sign in fragment

--- app/src/main/java/com/gonexwind/ourstory/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/gonexwind/ourstory/ui/auth/AuthActivity.kt
@@ -1,10 +1,10 @@
-package com.gonexwind.ourstory.ui.login
+package com.gonexwind.ourstory.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gonexwind.ourstory.R
 
-class LoginActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
