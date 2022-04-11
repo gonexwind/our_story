5fb8729d5a5f1fa0d1382fee2d118ce93811f1c5
gonexwind <fikky.ardi15@gmail.com>
2022-04-11 17:54:16 +0700

    fix custom view

--- app/src/main/java/com/gonexwind/ourstory/ui/auth/customview/PasswordEditText.kt
+++ app/src/main/java/com/gonexwind/ourstory/ui/auth/customview/PasswordEditText.kt
@@ -1,14 +1,26 @@
 package com.gonexwind.ourstory.ui.auth.customview
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.gonexwind.ourstory.R
 
-class PasswordEditText : AppCompatEditText {
+class CustomEditText : AppCompatEditText, View.OnTouchListener {
+    private lateinit var clearButtonImage: Drawable
+
     constructor(context: Context) : super(context) {
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
@@ -16,5 +28,89 @@ class PasswordEditText : AppCompatEditText {
         attrs,
         defStyleAttr
     ) {
+        init()
+    }
+
+    private fun init() {
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_cancel) as Drawable
+
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Do nothing.
+            }
+        })
+    }
+
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearButtonImage)
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = getDrawable(context, R.drawable.ic_password),
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_cancel) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_cancel) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else return false
+        }
+        return false
     }
 }
